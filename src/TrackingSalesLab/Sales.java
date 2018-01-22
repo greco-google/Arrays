4c5482a427955db6a92a8a99520d4fcfc455d34a
Josh Greco <greco.joshua@gmail.com>
2018-01-21 19:40:24 -0600

    TrackingSales Lab updated

--- src/TrackingSalesLab/Sales.java
+++ src/TrackingSalesLab/Sales.java
@@ -4,27 +4,27 @@ import java.util.Scanner;
 
 public class Sales {
 	public static void main(String[] args) {
-		final int SALESPEOPLE = 5;
-		int[] sales = new int[SALESPEOPLE];
 		int sum;
 		int maxSalesPerson = 0;
 		int maxSales = 0;
 		int minSalesPerson = 0;
 		int minSales = 0;
+
+		Scanner input = new Scanner(System.in);
+	    System.out.println("Enter the amout of people:");
+	    int SALESPEOPLE = input.nextInt();
+	    int sales[] = new int[SALESPEOPLE];
 		
-
-		Scanner scan = new Scanner(System.in);
-
 		for (int i = 0; i < sales.length; i++) {
-			System.out.print("Enter sales for salesperson " + i + ": ");
-			sales[i] = scan.nextInt();
+			System.out.print("Enter sales for salesperson " + (i + 1) + ": ");
+			sales[i] = input.nextInt();
 		}
 
 		System.out.println("\nSalesperson   Sales");
 		System.out.println("--------------------");
 		sum = 0;
 		for (int i = 0; i < sales.length; i++) {
-			System.out.println("     " + i + "         " + sales[i]);
+			System.out.println("     " + (i + 1) + "         " + sales[i]);
 			sum += sales[i];
 		}
 

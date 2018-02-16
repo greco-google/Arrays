25be262eeb3e0f6cd393791f15d0f7d609e9bc6e
Josh Greco <greco.joshua@gmail.com>
2018-02-16 13:36:15 -0600

    Working on the MasterOrder

--- src/GirlScoutCookieOrder/MasterOrder.java
+++ src/GirlScoutCookieOrder/MasterOrder.java
@@ -1,5 +1,42 @@
 package GirlScoutCookieOrder;
 
+import java.util.ArrayList;
+
 public class MasterOrder {
+	
+	private ArrayList <CookieOrder> orders = new ArrayList<>();
+	
+	//Default no argument constructor
+	public MasterOrder() {
+		
+	}
+	
+	public void addOrder(CookieOrder theOrder) {
+		orders.add(theOrder);
+	}
+	
+	public int getTotalBoxes() {
+		int totalBoxes = 0;
+		for(CookieOrder order: orders) {
+			totalBoxes += order.getNumBoxes();
+		}
+	return totalBoxes;
+		
+	}
+	
+	public void removeVariety(String variety) {
+		for(int i = orders.size(); i >= 0; i--) {
+			if(orders.get(i).getVariety().equals(variety)) {
+				orders.remove(i);
+			}
+		}
+	}
+	
+	public void showOrder() {
+		for(CookieOrder order: orders) {
+			System.out.println("Variety: " + order.getVariety() 
+			+ "Boxes: " + order.getNumBoxes());
+		}
+	}
 
 }

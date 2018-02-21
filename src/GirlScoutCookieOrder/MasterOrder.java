8546ce2ff302c876c3d2d91a0cbea1c97a17cab9
Josh Greco <greco.joshua@gmail.com>
2018-02-20 21:04:18 -0600

    Working very weel with flexible software

--- src/GirlScoutCookieOrder/MasterOrder.java
+++ src/GirlScoutCookieOrder/MasterOrder.java
@@ -35,8 +35,17 @@ public class MasterOrder {
 	public void showOrder() {
 		for(CookieOrder order: orders) {
 			System.out.println("Variety: " + order.getVariety() 
-			+ "Boxes: " + order.getNumBoxes());
+			+ " Boxes: " + order.getNumBoxes());
 		}
+	}
+
+	public void updateOrder(String variety, int numBoxes) {
+		for(int i = 0; i < orders.size(); i++) {
+			if(orders.get(i).getVariety().equals(variety)) {
+				orders.set(i, new CookieOrder(variety, numBoxes));
+			}
+		}
+		
 	}
 
 }
--- src/GirlScoutCookieOrder/OrderAppv2.java
+++ src/GirlScoutCookieOrder/OrderAppv2.java
@@ -11,7 +11,7 @@ public class OrderAppv2 {
 	MasterOrder myOrder = new MasterOrder();
 	CookieOrder order;
 	
-	System.out.println("Welcom to our Girl Scout Cookie Ordererer");
+	System.out.println("Welcome to our Girl Scout Cookie Ordererer");
 	System.out.println("Please select a task");
 	System.out.println("Press 1 to order cookies");
 	System.out.println("Press 2 to remove an order");
@@ -25,12 +25,51 @@ public class OrderAppv2 {
 		if(choice.equals("1")) {
 			System.out.println("What type of cookie would you like?");
 			System.out.println("[Thin Mints], [Tagalongs], [Samoas], [Dosidos]");
-			String cookieName = input.nextLine();
-			System.out.println("How many boxes of " + cookieName + " do you want?");
+			String variety = input.nextLine();
+			System.out.println("How many boxes of " + variety + " do you want?");
 			int numBoxes = input.nextInt();
-			order = new CookieOrder(cookieName, numBoxes);
+			input.nextLine();
+			order = new CookieOrder(variety, numBoxes);
 			myOrder.addOrder(order);
 			}
+		
+		
+		else if(choice.equals("2")) {
+			System.out.println("What type of cookie do you want to remove?");
+			System.out.println("[Thin Mints], [Tagalongs], [Samoas], [Dosidos]");
+			String variety = input.nextLine();
+			myOrder.removeVariety(variety);
+			}
+		
+		else if(choice.equals("3")) {
+			System.out.println("What type of cookie do you want to update?");
+			System.out.println("[Thin Mints], [Tagalongs], [Samoas], [Dosidos]");
+			String variety = input.nextLine();
+			System.out.println("Now, how many boxes of " + variety + " do you want?");
+			int numBoxes = input.nextInt();
+			input.nextLine();
+			myOrder.updateOrder(variety, numBoxes);
+
+			}
+		
+		
+		else if(choice.equals("4")) {
+			System.out.println();
+			System.out.println("Your Current Order: ");
+			myOrder.showOrder();
+			System.out.println();
+			}
+		
+		else if(choice.equals("5")) {
+			System.exit(0);
+			}
+		System.out.println("Please select another Task");
+		System.out.println("Press 1 to order cookies");
+		System.out.println("Press 2 to remove an order");
+		System.out.println("Press 3 to update an order");
+		System.out.println("Press 4 to view current order");
+		System.out.println("Press 5 to exit");
+		choice = input.nextLine();
 		}
 	}
 }

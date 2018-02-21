351ec72efdd13b4fc559337336fda482e41304b8
Josh Greco <greco.joshua@gmail.com>
2018-02-20 20:34:31 -0600

    adding one order to the stretch tasks

--- src/GirlScoutCookieOrder/OrderAppv2.java
+++ src/GirlScoutCookieOrder/OrderAppv2.java
@@ -9,6 +9,7 @@ public class OrderAppv2 {
 		Scanner input = new Scanner(System.in);
 
 	MasterOrder myOrder = new MasterOrder();
+	CookieOrder order;
 	
 	System.out.println("Welcom to our Girl Scout Cookie Ordererer");
 	System.out.println("Please select a task");
@@ -19,5 +20,17 @@ public class OrderAppv2 {
 	System.out.println("Press 5 to exit");
 	
 	String choice  = input.nextLine();
+	
+	while(!choice.equals("5")) {
+		if(choice.equals("1")) {
+			System.out.println("What type of cookie would you like?");
+			System.out.println("[Thin Mints], [Tagalongs], [Samoas], [Dosidos]");
+			String cookieName = input.nextLine();
+			System.out.println("How many boxes of " + cookieName + " do you want?");
+			int numBoxes = input.nextInt();
+			order = new CookieOrder(cookieName, numBoxes);
+			myOrder.addOrder(order);
+			}
+		}
 	}
 }

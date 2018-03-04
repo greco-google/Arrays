725a10a964cea81c58987c650bba8bc56472c626
Josh Greco <greco.joshua@gmail.com>
2018-03-04 11:19:29 -0600

    For the multiple choice and the true false

--- src/Exam/Ch6Exam.java
+++ src/Exam/Ch6Exam.java
@@ -1,5 +1,24 @@
 package Exam;
 
+import java.util.ArrayList;
+
 public class Ch6Exam {
 
+public static void main(String[] args) {
+		
+		//Raw Type (not recommended) Java 1.2 - 1.5 when Generics were introduced
+		//ArrayList numbers = new ArrayList();
+		
+		//Use Generics and assign a type with <> diamond operator
+		ArrayList<Integer> values = new ArrayList<>();
+		
+		int[]values1 = {9, 4, 12, 2, 4, 8, 18};
+			for(int i = 0; i < values1.length; i++) {
+				values1[i] += 1;
+			}
+		System.out.println(values1[4]);
+	}
 }
+//[I@75b84c92
+//[I@75b84c92
+//[I@75b84c92

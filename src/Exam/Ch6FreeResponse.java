8f8204e7f9635ac400dacefd63d443dbc391980d
Josh Greco <greco.joshua@gmail.com>
2018-03-04 19:29:47 -0600

    Getting back to working on the Free Response Questions

--- src/Exam/Ch6FreeResponse.java
+++ src/Exam/Ch6FreeResponse.java
@@ -5,20 +5,44 @@ import java.util.ArrayList;
 public class Ch6FreeResponse {
 	public static void main(String[] args) {
 		
-		//#25
-		public double computeAverage(int[] values, int elements) {
-			
-			double sum = 0;
-			int[] elements1 = {2, 4, 11, 27, 5};
-			    for(int i = 0; i < elements1.length; i++) { 
-			    	sum += elements1[i];
-			    //	sum = sum + elements1[i]; 
-			    double average = sum / values.length;
-			    
-			    		//System.out.println(sum);
-			    System.out.println("Average value of array element is " + average);
-			    }
-		}
+//		//#25
+//		public double computeAverage(int[] values, int elements) {
+//			
+//			double sum = 0;
+//			int[] elements1 = {2, 4, 11, 27, 5};
+//			    for(int i = 0; i < elements1.length; i++) { 
+//			    sum = sum + elements1[i]; 
+//			    double average = sum / values.length;
+//			    
+//			    		//System.out.println(sum);
+//			    System.out.println("Average value of array element is " + average);
+//			    }
+//		}
+		
+//		//#26
+//		int [] numbers = new int [5];
+//		int max = Integer.MIN_VALUE;// Start at smallest possible number
+//		int min = Integer.MAX_VALUE;// Start at largest possible number
+//		
+//		//initializer lists (build values right into the array)
+//		int [] someArray = {5,3,1,6,8,120,8,3};
+//		
+//		//find max and min
+//		
+//		for(int i = 0; i < someArray.length; i++) {
+//			if(someArray[i] > max) {
+//				max = someArray[i];
+//			}
+//			if(someArray[i] < min) {
+//				min = someArray[i];
+//			}
+//		}
+//			System.out.println("Max # found is " + max);
+//			System.out.println("Min # found is " + min);
+		
+		//#27
+		
+		
 		
 		
 		

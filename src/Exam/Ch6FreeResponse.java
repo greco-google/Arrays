a55ece3b328bd90538a9e992a2a07581e94392f5
Josh Greco <greco.joshua@gmail.com>
2018-03-04 19:22:33 -0600

    Getting back to working on the Free Response Questions

--- src/Exam/Ch6FreeResponse.java
+++ src/Exam/Ch6FreeResponse.java
@@ -1,5 +1,50 @@
 package Exam;
 
-public class Ch6FreeResponse {
+import java.util.ArrayList;
 
+public class Ch6FreeResponse {
+	public static void main(String[] args) {
+		
+		//#25
+		public double computeAverage(int[] values, int elements) {
+			
+			double sum = 0;
+			int[] elements1 = {2, 4, 11, 27, 5};
+			    for(int i = 0; i < elements1.length; i++) { 
+			    	sum += elements1[i];
+			    //	sum = sum + elements1[i]; 
+			    double average = sum / values.length;
+			    
+			    		//System.out.println(sum);
+			    System.out.println("Average value of array element is " + average);
+			    }
+		}
+		
+		
+		
+//		//#28
+		//public static void main(String[] args) {
+//		ArrayList<String> ChocolateOrders = new ArrayList();
+//		
+//		ChocolateOrders.add("Milk Chocolate");
+//		ChocolateOrders.add("Caramel");
+//		ChocolateOrders.add("Cookies and Cream");
+//		ChocolateOrders.add("Caramel");
+//		ChocolateOrders.add("Milk Chocolate");
+//		
+//		for (String ChocolateOrder : ChocolateOrders) {
+//			System.out.println(ChocolateOrder);
+//		}
+//
+//		
+//		ChocolateOrders.remove(1);
+//		ChocolateOrders.remove(2);
+//		
+//		System.out.println("\nThe new list of chocolates is:");
+//		for (String ChocolateOrder : ChocolateOrders) {
+//			System.out.println("\t" + ChocolateOrder);
+//		}
+		
+	}
 }
+
